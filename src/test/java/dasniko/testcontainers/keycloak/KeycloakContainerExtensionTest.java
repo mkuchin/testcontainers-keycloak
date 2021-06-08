ac53a196f1c0e3e0f67e223bad5650105be76fe2
Niko Köbler <niko@n-k.de>
2021-06-08 22:45:30 +0200

    migrate tests to junit-jupiter (#25)

--- src/test/java/dasniko/testcontainers/keycloak/KeycloakContainerExtensionTest.java
+++ src/test/java/dasniko/testcontainers/keycloak/KeycloakContainerExtensionTest.java
@@ -1,7 +1,7 @@
 package dasniko.testcontainers.keycloak;
 
 import dasniko.testcontainers.keycloak.extensions.oidcmapper.TestOidcProtocolMapper;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.keycloak.TokenVerifier;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.resource.RealmResource;
@@ -18,10 +18,9 @@ import java.util.Map;
 import static dasniko.testcontainers.keycloak.KeycloakContainerTest.ADMIN_CLI;
 import static dasniko.testcontainers.keycloak.KeycloakContainerTest.MASTER;
 import static dasniko.testcontainers.keycloak.KeycloakContainerTest.TEST_REALM_JSON;
-
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.startsWith;
-import static org.junit.Assert.assertThat;
 
 public class KeycloakContainerExtensionTest {
 
@@ -35,8 +34,6 @@ public class KeycloakContainerExtensionTest {
 
     /**
      * Deploys the Keycloak extensions from the test-classes folder into the create Keycloak container.
-     *
-     * @throws Exception
      */
     @Test
     public void shouldDeployExtension() throws Exception {
@@ -72,9 +69,6 @@ public class KeycloakContainerExtensionTest {
 
     /**
      * Configures the {@link TestOidcProtocolMapper} to the given client in the given realm.
-     *
-     * @param realm
-     * @param client
      */
     static void configureCustomOidcProtocolMapper(RealmResource realm, ClientRepresentation client) {
 
